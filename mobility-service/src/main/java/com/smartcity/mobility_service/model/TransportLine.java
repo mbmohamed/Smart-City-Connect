57e5a3fccdc70d0ca4c72cdc905e8b58c9c88d9c
Mohamed Ben Madhi <hammabenmadhi@gmail.com>
2025-12-04 03:12:20 +0100

    Refactor: Convert TransportLine entity to standard POJO

--- mobility-service/src/main/java/com/smartcity/mobility_service/model/TransportLine.java
+++ mobility-service/src/main/java/com/smartcity/mobility_service/model/TransportLine.java
@@ -4,14 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TransportLine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +14,46 @@ public class TransportLine {
     private String name;
     private String type; // BUS, TRAIN, METRO
     private String status; // ON_TIME, DELAYED, CANCELLED
+
+    public TransportLine() {
+    }
+
+    public TransportLine(Long id, String name, String type, String status) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }

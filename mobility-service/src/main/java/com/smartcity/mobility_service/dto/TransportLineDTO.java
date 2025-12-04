7f79475d97e132998260b829cbffc3fa9bc2d384
Mohamed Ben Madhi <hammabenmadhi@gmail.com>
2025-12-04 03:11:55 +0100

    Refactor: Convert TransportLineDTO to Java Record

--- mobility-service/src/main/java/com/smartcity/mobility_service/dto/TransportLineDTO.java
+++ mobility-service/src/main/java/com/smartcity/mobility_service/dto/TransportLineDTO.java
@@ -1,15 +1,4 @@
 package com.smartcity.mobility_service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class TransportLineDTO {
-    private Long id;
-    private String name;
-    private String type;
-    private String status;
+public record TransportLineDTO(Long id, String name, String type, String status) {
 }

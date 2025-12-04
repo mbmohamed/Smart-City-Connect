41813114db29062932ab2b608bed36d31723e35f
Mohamed Ben Madhi <hammabenmadhi@gmail.com>
2025-12-04 03:02:17 +0100

    Initialize mobility-service with Spring Boot 3.4.0

---
+++ mobility-service/src/main/java/com/smartcity/mobility_service/MobilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.mobility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityServiceApplication.class, args);
+	}
+
+}
